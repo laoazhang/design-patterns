c7c5775669391a803420a4bbb7cd5c3cf6289f9d
laoazhang <laoazhang@163.com>
2024-04-05 23:55:27 +0800

    feat: composite

---
+++ src/cn/laoazhang/composite/File.java
@@ -0,0 +1,28 @@
+package cn.laoazhang.composite;
+
+/**
+ * @Author laoazhang
+ * @CreateTime 2024/4/5 23:47
+ * @Version 1.0
+ */
+public class File extends Root{
+
+    public File(String name) {
+        super(name);
+    }
+
+    @Override
+    public void addFile(Root root) {
+
+    }
+
+    @Override
+    public void removeFile(Root root) {
+
+    }
+
+    @Override
+    public void display(int root) {
+
+    }
+}
